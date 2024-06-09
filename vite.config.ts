ed29f2df35b425ddfa6e91519bbc978c3ea4e023
Kauane Vieira <89592039+kauanevieira@users.noreply.github.com>
2024-06-09 20:11:56 +0000

    Versioning untracked files

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import path from "node:path"
-import react from '@vitejs/plugin-react'
+import path from 'node:path'
 
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
